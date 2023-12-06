64324b90f8f2a8bdbf1ff0c9844d9e4db34d6fc7
meitalcooper <118601362+meitalcooper@users.noreply.github.com>
2023-12-06 19:22:31 +0200

    Update ball.py

--- ball.py
+++ ball.py
@@ -13,19 +13,23 @@ class Ball(Turtle):
 
   
     def move(self):
+        """Move the ball in the current direction by updating its x and y coordinates."""
         new_x = self.xcor() + self.x_move
         new_y = self.ycor() + self.y_move
         self.goto(new_x, new_y)
 
     def bounce_y(self):
+        """Invert the y-axis movement direction."""
        self.y_move *= -1
-       self.move_speed *= 0.5
+       
     
     def bounce_x(self):
+        """Invert the x-axis movement direction and slightly increase the movement speed."""
         self.x_move *= -1
         self.move_speed *= 0.9
 
     def reset_position(self):
+        """Reset the ball's position to the center and revert its movement speed to the initial state."""
         self.move_speed = 0.1
         self.goto(0, 0)
         self.bounce_x()
